221e39f65cd00c051fe9a2a2d69677270bf1fb78
Mike Hamer <mike@mikehamer.info>
2016-11-25 21:51:24 +0100

    Sending external position measurements
    Signed-off-by: Mike Hamer <mike@mikehamer.info>

--- crazyflie/setpoint.go
+++ crazyflie/setpoint.go
@@ -14,3 +14,17 @@ func (cf *Crazyflie) SetpointSend(roll, pitch, yawrate float32, thrust uint16) {
 
 	cf.commandQueue <- packet // schedule transmission of the packet
 }
+
+func (cf *Crazyflie) ExternalPositionSend(x, y, z float32) {
+
+	// the packet to initialize the transaction
+	packet := make([]byte, 1+3*4)
+	packet[0] = crtp(crtpPortPosition, 0)
+	copy(packet[1:5], float32ToBytes(x))
+	copy(packet[5:9], float32ToBytes(y))
+	copy(packet[9:13], float32ToBytes(z))
+
+	// don't wait for a callback / acknowledgement, just send and be done with it
+
+	cf.commandQueue <- packet // schedule transmission of the packet
+}
