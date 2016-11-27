c15edb92be26013f3d790771bd166691fd71215b
Arnaud Taffanel <arnaud@bitcraze.io>
2016-11-27 18:16:50 +0100

    Added flag for radio channel and address
    Also modified example code for sending setpoint

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 
 func main() {
 	var err error
+	channel := flag.Uint("channel", 80, "Radio channel")
+	address := flag.Uint64("address", 0xE7E7E7E701, "Radio address")
 	flag.Parse()
 	cache.Init()
 
@@ -22,7 +24,9 @@ func main() {
 	}
 	defer radio.Close()
 
-	cf, err := crazyflie.Connect(radio, 0xE7E7E7E701)
+	radio.SetChannel(uint8(*channel))
+
+	cf, err := crazyflie.Connect(radio, *address)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,8 +54,20 @@ func main() {
 	val, err = cf.ParamRead("kalman.pNAcc_xy")
 	fmt.Println(val)
 
-	cf.SetpointSend(0, 0, 0, 4000)
-	<-time.After(1 * time.Second)
+	// Unlock commander
+	cf.SetpointSend(0, 0, 0, 0)
+	// Commander packets needs to be sent at regular interval, otherwise the
+	// commander watchdog will cut the motors
+	stop := false
+	go func() {
+		for !stop {
+			cf.SetpointSend(0, 0, 0, 4000)
+			<-time.After(20 * time.Millisecond)
+		}
+	}()
+	<-time.After(5 * time.Second)
+	stop = true
+	<-time.After(40 * time.Millisecond)
 	cf.SetpointSend(0, 0, 0, 0)
 	<-time.After(1 * time.Second)
 }
